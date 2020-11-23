ccb897d385b48e30a3b6e21e4d0f534ca09fd297
xtryfix <70887514+xtryfix@users.noreply.github.com>
2020-11-23 10:43:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru"> 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="id=edge">
+    <meta name="keywords" content="вёрстка, HTML, CSS, блог">
+    <meta name="description" content="Сайт начинающего программиста: Магазин на диване">
+    <title>QUIKSILVER</title>
+    <link rel="icon" href="assets/img/logo32.svg" sizes="32x32" type="image/png">
+    <link href="assets/css/style.css" type="text/css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+</head>
+<body class="container-fluid page light-theme">
+<?php
+require('code/header.php'); 
+require('code/aside1.php');
+require('code/aside2.php');
+?>
+<main>
+<div class="text-center">
+<p>Привет</p>
+</div>
+</main>
+<?php
+require('code/footer.php');
+?>
+</body>
+<script src="assets/js/theme.js"></script>
